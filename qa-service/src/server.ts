e49f4838d519ea0606fd660619f61fbe949f134a
hatimabdullah <hatimalqiamh@gmail.com>
2021-12-11 02:44:12 +0300

    fix: module path

--- qa-service/src/server.ts
+++ qa-service/src/server.ts
@@ -2,7 +2,7 @@ process.env['NODE_CONFIG_DIR'] = __dirname + '/configs';
 
 import 'dotenv/config';
 import 'reflect-metadata';
-import App from '@/app';
+import App from './app';
 import { AuthController } from './controllers/core/auth.controller';
 import { IndexController } from './controllers/core/index.controller';
 import { UsersController } from './controllers/core/users.controller';
